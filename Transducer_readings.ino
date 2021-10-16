1c2dd4541fdbc3c7bc8cc36218f435da193d9ffd
Ethan-Clark1 <90979050+Ethan-Clark1@users.noreply.github.com>
2021-10-16 10:58:19 -0400

    Add files via upload

--- Transducer_readings.ino
+++ Transducer_readings.ino
@@ -1,2 +1,10 @@
 
-// For readings of pressures
+// For readings of pressures and logging them
+
+void logPressures() {
+  // Write pressures to serial monitor
+  Serial.begin(9600);
+  Serial.write(oxPressure);
+  Serial.write(fuelPressure);
+ 
+}
