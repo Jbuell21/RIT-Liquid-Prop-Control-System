53e9315362dfe5a3da838b5a335f87a06314f50a
Ethan-Clark1 <90979050+Ethan-Clark1@users.noreply.github.com>
2022-01-16 00:50:03 -0500

    Add files via upload

--- Teensy_main/Packet_parse.ino
+++ Teensy_main/Packet_parse.ino
@@ -12,9 +12,11 @@ void packetRead {                     // Read incoming packet and parse it
       if(packetBuffer.state == 0) {
         replyBuffer[0] = 0;
         digitalWrite(oxValve.pin, HIGH);
+        stateUpdate(oxValve, 0);
       } else {
         replyBuffer[0] = 1;
         digitalWrite(oxValve.pin, LOW);
+        stateUpdate(oxValve, 1);
       }
       break;
 
@@ -22,68 +24,107 @@ void packetRead {                     // Read incoming packet and parse it
       if(packetBuffer.state == 0){
         replyBuffer[1] = 0;
         digitalWrite(fuelValve.pin, HIGH);
+        stateUpdate(fuelValve, 0);
       } else {
         replyBuffer[1] = 1;
         digitalWrite(fuelValve.pin, LOW);
+        stateUpdate(fuelValve, 1);
       }
+      break;
+
     case purgeValve.controlNum:
       if(packetBuffer.state == 0){
         replyBuffer[2] = 0;
         digitalWrite(purgeValve.pin, HIGH);
+        stateUpdate(purgeValve, 0);
       } else {
         replyBuffer[2] = 1;
         digitalWrite(purgeValve.pin, LOW);
+        stateUpdate(purgeValve, 1);
       }
+      break;
+
     case igniter.controlNum:
       if(packetBuffer.state == 0){
         replyBuffer[3] = 0;
         digitalWrite(igniter.pin, HIGH);
+        stateUpdate(igniter, 0);
       } else {
         replyBuffer[3] = 1;
         digitalWrite(igniter.pin, LOW);
+        stateUpdate(igniter, 1);
       }
+      break;
+
     case redLight.controlNum:
       if(packetBuffer.state == 0){
         replyBuffer[4] = 0;
         digitalWrite(redLight.pin, HIGH);
+        stateUpdate(redLight, 0);
       } else {
         replyBuffer[4] = 1;
         digitalWrite(redLight.pin, LOW);
+        stateUpdate(redLight, 1);
       }
+      break;
+
     case yellowLight.controlNum:
       if(packetBuffer.state == 0){
         replyBuffer[5] = 0;
         digitalWrite(yellowLight.pin, HIGH);
+        stateUpdate(yellowLight, 0);
       } else {
         replyBuffer[5] = 1;
         digitalWrite(yellowLight.pin, LOW);
+        stateUpdate(yellowLight, 1);
       }
+      break;
+
     case greenLight.controlNum:
       if(packetBuffer.state == 0){
         replyBuffer[6] = 0;
         digitalWrite(greenLight.pin, HIGH);
+        stateUpdate(greenLight, 0);
       } else {
         replyBuffer[6] = 1;
         digitalWrite(greenLight.pin, LOW);
+        stateUpdate(greenLight, 1);
       }
+      break;
+
     case buzzer.controlNum:
       if(packetBuffer.state == 0){
         replyBuffer[7] = 0;
         digitalWrite(buzzer.pin, HIGH);
+        stateUpdate(buzzer, 0);
       } else {
         replyBuffer[7] = 1;
         digitalWrite(buzzer.pin, LOW);
+        stateUpdate(buzzer, 1);
       }
-  }
+      break;
 
+  }
+    default;
 }
 
-void packetSend {                      // Send packet back with response and telemetry
-
-
-
+void packetSend(packetBuffer) {                      // Send packet back with response and telemetry
+  packetRead();
+  if(packetBuffer.controlNum >= 100 && packetBuffer.controlNum < 200){
+    Udp.begin(Udp.remoteIP(), 8080);
+    Udp.write(replyBuffer);
+    Udp.endPacket();
+  } else if(packetBuffer.controlNum >= 200 && packetBuffer.controlNum < 300) {
+    Udp.begin(Udp.remoteIP(), 8081);
+    Udp.write(replyBuffer);
+    Udp.endPacket();
+  } else {
+    Udp.begin(Udp.remoteIP(), 8082);
+    Udp.write(replyBuffer);
+    Udp.endPacket();
+  }
 }
 
 void stateUpdate(object, stateNew) {                     // Update states of things
-
+  object.currentState == stateNew;
 }
--- Teensy_main/Teensy_main.ino
+++ Teensy_main/Teensy_main.ino
@@ -2,7 +2,7 @@
 #include <NativeEthernet.h>
 #include <NativeEthernetUdp.h>
 
-uint16_t currentSeqNum = 0
+uint16_t currentSeqNum = 0;
 char packetBuffer[8];  // buffer to hold incoming packet,
 
 byte mac[] = {
@@ -33,7 +33,67 @@ typedef struct{
   uint16_t buzz;
 } tower_t;
 
+relay_t oxValve;
+oxValve.pinNum = 0;
+oxValve.currentState = 0;
+oxValve.controlNum = 100;
+
+relay_t fuelValve;
+fuelValve.pinNum = 1;
+fuelValve.currentState = 0;
+fuelValve.controlNum = 101;
+
+relay_t purgeValve;
+purgeValve.pinNum = 2;
+purgeValve.currentState = 0;
+purgeValve.controlNum = 102;
+
+relay_t igniter;
+igniter.pinNum = 3;
+igniter.currentState = 0;
+igniter.controlNum = 200;
+
+relay_t redLight;
+redLight.pinNum = 4;
+redLight.currentState = 0;
+redLight.controlNum = 300;
+
+relay_t yellowLight;
+yellowLight.pinNum = 5;
+yellowLight.currentState = 0;
+yellowLight.controlNum = 301;
+
+relay_t greenLight;
+greenLight.pinNum = 6;
+greenLight.currentState = 0;
+greenLight.controlNum = 302;
+
+relay_t buzzer;
+buzzer.pinNum = 7;
+buzzer.currentState = 0;
+buzzer.controlNum = 303;
+
+uint16_t replyBuffer[8] = [0, 0, 0, 0, 0, 0, 0, 0]
+
 void setup() {
+  pinMode(oxValve, OUTPUT);
+  digitalWrite(oxValve, HIGH);
+  pinMode(fuelValve, OUTPUT);
+  digitalWrite(fuelValve, HIGH);
+  pinMode(purgeValve, OUTPUT);
+  digitalWrite(purgeValve, HIGH);
+  pinMode(igniter, OUTPUT);
+  digitalWrite(igniter, HIGH);
+
+  pinMode(redLight, OUTPUT);
+  digitalWrite(redLight, HIGH);
+  pinMode(yellowLight, OUTPUT);
+  digitalWrite(yellowLight, HIGH);
+  pinMode(greenLight, OUTPUT);
+  digitalWrite(greenLight, HIGH);
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, HIGH);
+
   Ethernet.begin(mac, ip);
 
   if(Ethernet.hardwareStatus() == EthernetNoHardware){
@@ -51,9 +111,7 @@ void setup() {
 void loop() {
   int packetSize = Udp.parsePacket();
   if (packetSize) {
-    if (content.seq_num) {
-
-    }
+    
   }
 
 
